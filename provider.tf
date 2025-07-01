68b9f5a58e7ec2843bedc47efc4c1d843c913b24
yossij <yossijana@gmail.com>
2025-07-02 01:33:16 +0300

    add S3 backend configuration in provider.tf

--- provider.tf
+++ provider.tf
@@ -12,11 +12,10 @@ terraform {
     }
   }
 
-  # optional - if you want terraform state bucket
-  # backend "s3" {
-  #   bucket  = var.terraform_state_bucket
-  #   key     = var.terraform_state_key
-  #   region  = var.region
-  #   profile = var.profile
-  # }
+  backend "s3" {
+    bucket  = "anysource-infra-tfstate-j8cv7ypcdboj"
+    key     = "anysource-production-infra/terraform.tfstate"
+    region  = "us-east-1"
+    profile = var.profile
+  }
 }
